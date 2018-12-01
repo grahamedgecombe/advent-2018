8e3817d54c9dedbfea4622faf5bace7104568ba6
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-12-01 10:01:15 +0000

    Add day 1 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2018/Day1.java
+++ src/main/java/com/grahamedgecombe/advent2018/Day1.java
@@ -1,15 +1,33 @@
 package com.grahamedgecombe.advent2018;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class Day1 {
 	public static void main(String[] args) throws IOException {
 		List<String> lines = AdventUtils.readLines("day1.txt");
 		System.out.println(getFrequency(lines));
+		System.out.println(getFrequencySeenTwice(lines));
 	}
 
 	public static int getFrequency(List<String> lines) {
 		return lines.stream().mapToInt(Integer::parseInt).sum();
+	}
+
+	public static int getFrequencySeenTwice(List<String> lines) {
+		int[] changes = lines.stream().mapToInt(Integer::parseInt).toArray();
+
+		int frequency = 0;
+
+		Set<Integer> seenFrequencies = new HashSet<>();
+		seenFrequencies.add(frequency);
+
+		int i = 0;
+		do {
+			frequency += changes[i++ % changes.length];
+		} while (seenFrequencies.add(frequency));
+		return frequency;
 	}
 }
--- src/test/java/com/grahamedgecombe/advent2018/Day1Test.java
+++ src/test/java/com/grahamedgecombe/advent2018/Day1Test.java
@@ -14,4 +14,13 @@ public final class Day1Test {
 		assertEquals(0, Day1.getFrequency(Arrays.asList("+1", "+1", "-2")));
 		assertEquals(-6, Day1.getFrequency(Arrays.asList("-1", "-2", "-3")));
 	}
+
+	@Test
+	public void testPart2() {
+		assertEquals(2, Day1.getFrequencySeenTwice(Arrays.asList("+1", "-2", "+3", "+1")));
+		assertEquals(0, Day1.getFrequencySeenTwice(Arrays.asList("+1", "-1")));
+		assertEquals(10, Day1.getFrequencySeenTwice(Arrays.asList("+3", "+3", "+4", "-2", "-4")));
+		assertEquals(5, Day1.getFrequencySeenTwice(Arrays.asList("-6", "+3", "+8", "+5", "-6")));
+		assertEquals(14, Day1.getFrequencySeenTwice(Arrays.asList("+7", "+7", "-2", "-7", "-4")));
+	}
 }
