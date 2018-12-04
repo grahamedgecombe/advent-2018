9902d27f0d66ee401390436dce58f0712e7f870e
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-12-04 19:35:16 +0000

    Add day 4 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2018/Day4.java
+++ src/main/java/com/grahamedgecombe/advent2018/Day4.java
@@ -13,9 +13,9 @@ public final class Day4 {
 	private static final Pattern EVENT_PATTERN = Pattern.compile("^\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})] (wakes up|falls asleep|Guard #([0-9]+) begins shift)$");
 
 	public static void main(String[] args) throws IOException {
-		List<String> lines = AdventUtils.readLines("day4.txt");
-		Collections.sort(lines);
-		System.out.println(getSleepiestGuard(lines));
+		Map<Integer, Guard> guards = parseGuards(AdventUtils.readLines("day4.txt"));
+		System.out.println(getSleepiestGuard(guards));
+		System.out.println(getSleepiestMinute(guards));
 	}
 
 	private static final class Guard {
@@ -52,12 +52,18 @@ public final class Day4 {
 
 			return maxKey;
 		}
+
+		public int getSleepiestMinuteFrequency() {
+			int sleepiestMinute = getSleepiestMinute();
+			return minutes[sleepiestMinute];
+		}
 	}
 
-	public static int getSleepiestGuard(List<String> lines) {
+	public static Map<Integer, Guard> parseGuards(List<String> lines) {
 		Map<Integer, Guard> guards = new HashMap<>();
 		int guardId = 0, fellAsleepAt = 0;
 
+		Collections.sort(lines);
 		for (String line : lines) {
 			Matcher matcher = EVENT_PATTERN.matcher(line);
 			if (!matcher.matches()) {
@@ -81,9 +87,21 @@ public final class Day4 {
 			}
 		}
 
+		return guards;
+	}
+
+	public static int getSleepiestGuard(Map<Integer, Guard> guards) {
 		Guard guard = guards.values()
 			.stream()
 			.max(Comparator.comparing(Guard::getTotalMinutes))
+			.get();
+		return guard.id * guard.getSleepiestMinute();
+	}
+
+	public static int getSleepiestMinute(Map<Integer, Guard> guards) {
+		Guard guard = guards.values()
+			.stream()
+			.max(Comparator.comparing(Guard::getSleepiestMinuteFrequency))
 			.get();
 		return guard.id * guard.getSleepiestMinute();
 	}
--- src/test/java/com/grahamedgecombe/advent2018/Day4Test.java
+++ src/test/java/com/grahamedgecombe/advent2018/Day4Test.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public final class Day4Test {
 	@Test
 	public void testPart1() {
-		assertEquals(240, Day4.getSleepiestGuard(Arrays.asList(
+		assertEquals(240, Day4.getSleepiestGuard(Day4.parseGuards(Arrays.asList(
 			"[1518-11-01 00:00] Guard #10 begins shift",
 			"[1518-11-01 00:05] falls asleep",
 			"[1518-11-01 00:25] wakes up",
@@ -27,6 +27,29 @@ public final class Day4Test {
 			"[1518-11-05 00:03] Guard #99 begins shift",
 			"[1518-11-05 00:45] falls asleep",
 			"[1518-11-05 00:55] wakes up"
-		)));
+		))));
+	}
+
+	@Test
+	public void testPart2() {
+		assertEquals(4455, Day4.getSleepiestMinute(Day4.parseGuards(Arrays.asList(
+			"[1518-11-01 00:00] Guard #10 begins shift",
+			"[1518-11-01 00:05] falls asleep",
+			"[1518-11-01 00:25] wakes up",
+			"[1518-11-01 00:30] falls asleep",
+			"[1518-11-01 00:55] wakes up",
+			"[1518-11-01 23:58] Guard #99 begins shift",
+			"[1518-11-02 00:40] falls asleep",
+			"[1518-11-02 00:50] wakes up",
+			"[1518-11-03 00:05] Guard #10 begins shift",
+			"[1518-11-03 00:24] falls asleep",
+			"[1518-11-03 00:29] wakes up",
+			"[1518-11-04 00:02] Guard #99 begins shift",
+			"[1518-11-04 00:36] falls asleep",
+			"[1518-11-04 00:46] wakes up",
+			"[1518-11-05 00:03] Guard #99 begins shift",
+			"[1518-11-05 00:45] falls asleep",
+			"[1518-11-05 00:55] wakes up"
+		))));
 	}
 }
